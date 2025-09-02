2db9973ff0ff42d375aa55154135b2d9ced42203
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-09-02 02:48:16 -0300

    ADD: Cabeçalho e correções no código C

--- Aulas-praticas/aula0101.c
+++ Aulas-praticas/aula0101.c
@@ -1,10 +1,31 @@
+/*
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e de Computacao
+ * EEL270 - Computacao II - Turma 2025/2
+ * Prof. Marcelo Luiz Drumond Lanza
+ * Autor: Danilo Davi Gomes Froes
+ *
+ * Descricao:
+ * 	Exibir as 256 cores possiveis em um terminal.
+ *
+ * $Author$
+ * $Date$
+ * $Log$
+ */
+
 #include <stdio.h>
 
 int main() {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i < 255; i++) {
-        printf("\e[38,5,%dm", i);
-        printf("\e[0m");
-    }
+	for (i = 0; i < 256; i++) {
+		printf("\e[38;5;%dm%3d", i, i);
+	}
+
+	printf("\e[0m\n");
+
+	return 0;
 }
+
+/* $RCSfile$ */
