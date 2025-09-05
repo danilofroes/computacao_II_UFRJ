4efa3b6b7ad8dddc55974fa1c46bbb846b4e4f9e
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-09-05 03:51:19 -0300

    ADD: Código completo aula0103

--- Aulas-praticas/aula0103.c
+++ Aulas-praticas/aula0103.c
@@ -22,7 +22,6 @@
 void ExibirCabecalho(unsigned short);
 
 void ExibirCabecalho(unsigned short numeroColunas) {
-    unsigned short i;
     unsigned short numeroLinhas;
 
     if (numeroColunas >=1 && numeroColunas <= 9)
@@ -36,10 +35,49 @@ void ExibirCabecalho(unsigned short numeroColunas) {
         
     else
         numeroLinhas = 4;
+
+    printf("%dm%dm", WHITE_BACKGROUND, BLACK);
+
+    unsigned short i;
+    for (i = 0; i < numeroLinhas; i++) {
+        unsigned short j;
+        for (j = 0; j < numeroColunas; j++) {
+            unsigned short digito = (j / 10) % 10;
+            printf("%u", digito);
+        }
+        printf("\n");
+    }
+
+    for (i = 0; i < numeroColunas; i++) {
+        unsigned short digito = i % 10;
+        printf("%u", digito);
+    }
+
+    printf("\n");
+
+    printf("%s", RESET);
 }
 
 int main(int argc, char *argv[]) {
+    unsigned short numeroColunas;
 
+    if(argc != 2) {
+        printf("%dmErro: Uso incorreto.%s\n", BOLD_RED, RESET);
+        printf("Uso: %s <numero_colunas>\n", argv[0]);
+        return 1;
+    }
+
+    numeroColunas = (unsigned short)atoi(argv[1]);
+
+    if(numeroColunas < 1 || numeroColunas > 200) {
+        printf("%dmErro: Numero de colunas invalido.%s\n", BOLD_RED, RESET);
+        printf("O numero de colunas deve estar entre 1 e 200.\n");
+        return 1;
+    }
+
+    ExibirCabecalho(numeroColunas);
+
+    return 0;
 }
 
 /* $RCSfile$ */
