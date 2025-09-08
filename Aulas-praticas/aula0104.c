26fac6a411be96cb57adc717cb0f7b769974d7fc
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-09-08 03:03:23 -0300

    REFACTOR: Terminando a aula0104

--- Aulas-praticas/aula0104.c
+++ Aulas-praticas/aula0104.c
@@ -7,7 +7,7 @@
  * Autor: Danilo Davi Gomes Froes
  *
  * Descricao:
- * 	  --
+*   Codigo para centralizar uma mensagem no terminal
  *
  * $Author$
  * $Date$
@@ -15,20 +15,67 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "cores.h"
+
+#define TAMANHO_LINHA 100
 
 int main(int argc, char *argv[]) {
     
-    if (argc >= 10) {
-        printf("O numero de argumentos passado e maior do que o aceito pelo funcao/n")
+    if (argc == 1 || argc > 11) {
+        printf("%sO numero de argumentos passado nao e aceito pela funcao%s\n", BOLD_HIGH_INTENSITY_RED, COLOR_RESET);
         return 1;
     }
 
-    if (strlen(argv[*]) > 50) {
-        printf("O numero de caracteres passado e maior que 50, que é o valor aceito pela funcao")
+    unsigned short quantidadeDeCaracteres;
+
+    if (argc > 2) {
+        unsigned short i;
+
+        for (i = 1; i < argc; i++)
+            quantidadeDeCaracteres += strlen(argv[i]);
+    }
+
+    else 
+        quantidadeDeCaracteres = strlen(argv[1]);
+        
+    if (quantidadeDeCaracteres > 50) {
+        printf("%sO numero de caracteres passado eh maior que 50, que eh o valor aceito pela funcao%s\n", BOLD_HIGH_INTENSITY_RED, COLOR_RESET);
         return 1;
     }
 
+    unsigned short espacosParaCentralizar;
+
+    if (quantidadeDeCaracteres % 2 == 0)
+        espacosParaCentralizar = (TAMANHO_LINHA - (quantidadeDeCaracteres + 2)) / 2;
+
+    else
+        espacosParaCentralizar = ((TAMANHO_LINHA - (quantidadeDeCaracteres + 2)) / 2) - 1;
+        
+    espacosParaCentralizar += (argc - 2);
+
+    unsigned short i;
+
+    for (i = 0; i <= espacosParaCentralizar; i++)
+        printf(" ");
+        
+    printf("\"");
     
+    for (i = 1; i < argc; i++) {
+        printf("%s", argv[i]);
+
+        if (argc > 2 && i != (argc - 1))
+            printf(" ");
+    }
+    
+    printf("\"");
+
+    for (i = 0; i <= espacosParaCentralizar; i++)
+        printf(" ");
+
+    return 0;
 }
 
  /* $RCSfile$ */
