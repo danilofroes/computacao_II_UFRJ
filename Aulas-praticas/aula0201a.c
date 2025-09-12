b3bd32a2300ca44bbb752fada51c8870431b6217
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-09-12 03:36:26 -0300

    FIX: Corrigindo versão do cálculo de MDC com recursiva

--- Aulas-praticas/aula0201a.c
+++ Aulas-praticas/aula0201a.c
@@ -7,7 +7,7 @@
  * Autor: Danilo Davi Gomes Froes
  *
  * Descricao:
- * 	  --
+ * 	  Arquivo de implementacao da funcao de calculo do maximo divisor comum (mdc) com recursao
  *
  * $Author$
  * $Date$
@@ -17,18 +17,20 @@
 #include "aula0201.h"
 
 ull CalcularMaximoDivisorComum(ull x, ull y) {
-    ull resto = x % y;
+    ull resto;
 
-    if (resto != 0)
-        return CalcularMaximoDivisorComum(y, resto);
-    
-    else if (resto == 0 || (x == 0 && y != 0))
-        return y;
-    
-    else if (y == 0 && x != 0)
+    if (y == 0)
         return x;
 
-    return 0;
+    else if (x == 0)
+        return y;
+
+    resto = x % y;
+
+    if (resto == 0)
+        return y;
+
+    return CalcularMaximoDivisorComum(y, resto);
 }
 
  /* $RCSfile$ */
