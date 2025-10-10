289915cc534cd800208ed72496934c40476c045f
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-10-10 01:29:28 -0300

    ADD: Função para calcular termo da serie de fibonacci com for

---
+++ Aulas-praticas/aula0301c.c
@@ -0,0 +1,54 @@
+/*
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e de Computacao
+ * EEL270 - Computacao II - Turma 2025/2
+ * Prof. Marcelo Luiz Drumond Lanza
+ * Autor: Danilo Davi Gomes Froes
+ *
+ * Descricao:
+ * 	  Arquivo de implementacao da funcao de calculo do termo da serie de fibonacci com for
+ *
+ * $Author$
+ * $Date$
+ * $Log$
+ */
+
+#if defined (__FreeBSD__) && defined (__STRICT_ANSI__)
+  #define __LONG_LONG_SUPPORTED
+#endif
+
+#ifdef __linux__
+  #define _XOPEN_SOURCE 600
+#endif
+
+#include "aula0301.h"
+#include <limits.h>
+
+ull CalcularTermoSerieFibonacci(us termo) {
+    ull termoAnterior;
+    ull termoAtual;
+    ull proximoTermo;
+    ull *ptrAnterior = &termoAnterior;
+    ull *ptrAtual = &termoAtual;
+    ull *ptrProximo = &proximoTermo;
+
+    if (termo <= 1)
+        return termo;
+
+    termoAnterior = 0;
+    termoAtual = 1;
+    
+    for (us i = 2; i <= termo; i++) {
+        if (ULLONG_MAX - *ptrAtual < *ptrAnterior)
+            return ULLONG_MAX;
+
+        *ptrProximo = *ptrAnterior + *ptrAtual;
+        *ptrAnterior = *ptrAtual;
+        *ptrAtual = *ptrProximo;
+    }
+
+    return *ptrAtual;
+}
+
+ /* $RCSfile$ */
