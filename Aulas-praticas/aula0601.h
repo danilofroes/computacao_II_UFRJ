31dca0363a5c4d59467e40b3fc0a3b7e25bd782a
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-10-30 01:46:57 -0300

    ADD: Aula0601.h

---
+++ Aulas-praticas/aula0601.h
@@ -0,0 +1,39 @@
+/*
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e de Computacao
+ * EEL270 - Computacao II - Turma 2025/2
+ * Prof. Marcelo Luiz Drumond Lanza
+ * Autor: Danilo Davi Gomes Froes
+ *
+ * Descricao:
+ * 	  --
+ *
+ * $Author$
+ * $Date$
+ * $Log$
+ */
+
+#ifndef _AULA0601_
+#define _AULA0601_ "@(#)aula0601.h $Revision$"
+
+typedef enum {
+    mastercard,
+    visa13,
+    visa14,
+    visa15,
+    visa16,
+    amex,
+    diner
+}
+tipoOperadora;
+
+typedef enum {
+    ok
+} tipoCodigosRetorno;
+
+tipoCodigosRetorno GerarDigitoVerificadorCartaoCredito(byte [] /* entrada/saida */);
+
+#endif
+
+ /* $RCSfile$ */
