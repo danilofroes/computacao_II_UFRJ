e3b533dcaa21557031eb6da3641c9bc8cadbe6d0
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2025-10-31 22:09:53 -0300

    ADD: Arquivo aula0601.c

---
+++ Aulas-praticas/aula0601.c
@@ -0,0 +1,23 @@
+/*
+ * Universidade Federal do Rio de Janeiro
+ * Escola Politecnica
+ * Departamento de Eletronica e de Computacao
+ * EEL270 - Computacao II - Turma 2025/2
+ * Prof. Marcelo Luiz Drumond Lanza
+ * Autor: Danilo Davi Gomes Froes
+ *
+ * Descricao:
+ * 	  --
+ *
+ * $Author$
+ * $Date$
+ * $Log$
+ */
+
+#include "aula0601.h"
+
+tipoCodigosRetorno GerarDigitoVerificadorCartaoCredito(byte digitosCartao[]) {
+    
+}
+
+ /* $RCSfile$ */
