4b2c1e49e52285a88216a825aadfad5bce05585a
Danilo Fróes <danilofroes.20241@poli.ufrj.br>
2026-01-09 18:47:25 -0300

    ADD: Implementando a função de gerar dígito do cartão

--- Aulas-praticas/aula0601.c
+++ Aulas-praticas/aula0601.c
@@ -7,7 +7,7 @@
  * Autor: Danilo Davi Gomes Froes
  *
  * Descricao:
- * 	  --
+ * 	  Implementacao da funcao de geracao de digito verificador para cartoes de credito.
  *
  * $Author$
  * $Date$
@@ -16,8 +16,40 @@
 
 #include "aula0601.h"
 
-tipoCodigosRetorno GerarDigitoVerificadorCartaoCredito(byte digitosCartao[]) {
-    
+tipoCodigosRetorno GerarDigitoVerificadorCartaoCredito (byte cartao[] /* entrada/saida */) {
+    unsigned short indice;
+    unsigned short contador;
+    unsigned short soma = 0;
+    unsigned short digito;
+    unsigned short tamanho = 0;
+
+    while (cartao[tamanho] != 255)
+        tamanho++;
+
+    if (tamanho < 12) 
+        return erroComprimentoInvalido;
+
+    for (contador = 0; contador < tamanho; contador++) {
+        indice = tamanho - 1 - contador;
+        digito = cartao[indice];
+
+        if (contador % 2 == 0) {
+            digito *= 2;
+
+            if (digito > 9)
+                digito -= 9;
+        }
+
+        soma += digito;
+    }
+
+    if (soma % 10 == 0)
+        cartao[tamanho] = 0;
+        
+    else
+        cartao[tamanho] = 10 - (soma % 10);
+
+    return ok;
 }
 
- /* $RCSfile$ */
+/* $RCSfile$ */
